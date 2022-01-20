da924c5d9d9e272e17ce75c8d3e6d771230a056a
have fun <wilsonfso@gmail.com>
2022-01-19 21:11:43 -0700

    Day5 For Loops and Range and Random methods

--- 100 Days of Python/password-generator.py
+++ 100 Days of Python/password-generator.py
@@ -9,23 +9,17 @@ nr_letters= int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
-#Eazy Level - Order not randomised:
-#e.g. 4 letter, 2 symbol, 2 number = JduE&!91
-password_letters = ''
+password = []
 for i in range(1,nr_letters + 1):
-    password_letters += letters[random.randint(0, len(letters) - 1)]
- 
-password_symbols = ''
+    password.append(random.choice(letters))
+
 for i in range(1,nr_symbols + 1):
-    password_symbols += symbols[random.randint(0, len(symbols) - 1)]
-
-password_numbers = ''
-for i in range(1,nr_numbers + 1):
-    password_numbers += numbers[random.randint(0, len(numbers) - 1)]
-
+    password.append(random.choice(symbols))
  
-final_password = list(password_letters + password_symbols + password_numbers) 
-random.shuffle(final_password)  
-print(''.join(final_password))
+for i in range(1,nr_numbers + 1):
+    password.append(random.choice(numbers))
+
+random.shuffle(password)  
+print(''.join(password))
 
  
